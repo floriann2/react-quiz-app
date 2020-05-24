6492ba8a682e6cc1fb73de2b8307a9ecf00e179a
unknown <floriann25@gmail.com>
2020-05-24 18:29:11 -0400

    Add the last dance photo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,14 @@
 import React, { Fragment } from 'react';
 import { Helmet } from 'react-helmet'; //to change page title
 import { Link } from 'react-router-dom';
-
+import bulls from '../assets/img/chip.jpg';
 const Home = () => (
 	<Fragment> 
 		<Helmet><title>Quiz App-Home</title></Helmet>
 
 		<div id= "home">
 			<section>
-				<h1>Quiz App</h1>
+				<h1>The Last Dance</h1>
 				<div className="play-button-container">
 					<ul>
 						<li><Link className="play-button" to="/play/instructions">Play</Link></li>
@@ -19,7 +19,9 @@ const Home = () => (
 					<Link to = "./login" className="auth-buttons" id="login-button">Login</Link>
 					<Link to = "./register" className="auth-buttons" id="signup-button">Register</Link>
 				</div>
-
+				<div className = "bulls-photo">
+					<img src = { bulls } alt = "bulls photo" />
+				</div>
 			</section>
 		</div>
 	</Fragment>
